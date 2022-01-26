731316e9e83457482316ecd45f0ef84c5e605b28
BigRedEye <mail@bigredeye.me>
2022-01-26 23:58:54 +0300

    feat(build): Add optional backward-cpp

--- cmake/dependencies/common.cmake
+++ cmake/dependencies/common.cmake
@@ -1,7 +1,12 @@
 include(FetchContent)
 
-### cpparg ###
 
+### threads ###
+find_package(Threads REQUIRED)
+list(APPEND POORPROF_PUBLIC_LIBRARIES Threads::Threads)
+
+
+### cpparg ###
 FetchContent_Declare(
     cpparg
     GIT_REPOSITORY https://github.com/BigRedEye/cpparg
@@ -9,14 +14,24 @@ FetchContent_Declare(
 )
 
 if(NOT cpparg_POPULATED)
-  FetchContent_Populate(cpparg)
-
-  # Bring the populated content into the build
-  add_subdirectory(${cpparg_SOURCE_DIR} ${cpparg_BINARY_DIR} EXCLUDE_FROM_ALL)
+    FetchContent_Populate(cpparg)
+    add_subdirectory(${cpparg_SOURCE_DIR} ${cpparg_BINARY_DIR} EXCLUDE_FROM_ALL)
 endif()
-
 list(APPEND POORPROF_PRIVATE_LIBRARIES cpparg::cpparg)
 
-### threads ###
-find_package(Threads REQUIRED)
-list(APPEND POORPROF_PUBLIC_LIBRARIES Threads::Threads)
+
+### backward-cpp ###
+if (POORPROF_ENABLE_BACKWARD)
+    FetchContent_Declare(
+        backward
+        GIT_REPOSITORY https://github.com/bombela/backward-cpp
+        GIT_TAG        5ffb2c879ebdbea3bdb8477c671e32b1c984beaa
+    )
+
+    if(NOT backward_POPULATED)
+        FetchContent_Populate(backward)
+        add_definitions(-DBACKWARD_HAS_DW=1)
+        add_subdirectory(${backward_SOURCE_DIR} ${backward_BINARY_DIR} EXCLUDE_FROM_ALL)
+    endif()
+    list(APPEND POORPROF_PRIVATE_LIBRARIES backward backward_object)
+endif()
--- cmake/variables.cmake
+++ cmake/variables.cmake
@@ -3,3 +3,5 @@ set(POORPROF_DEBUG_INFO_PROVIDER "libdwfl" CACHE STRING "Which debug info parser
 
 set(POORPROF_STATIC_LIBSTDCXX FALSE CACHE BOOL "Enable -static-libgcc -static-libstdc++")
 set(POORPROF_STATIC_LIBC FALSE CACHE BOOL "Enable -static")
+
+set(POORPROF_ENABLE_BACKWARD TRUE CACHE BOOL "Automatically fetch & link with backward-cpp")
