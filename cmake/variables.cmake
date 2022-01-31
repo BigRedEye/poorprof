9cd13e1cb799863272a7aa629eec9cdb90d9547a
BigRedEye <mail@bigredeye.me>
2022-02-01 02:51:02 +0300

    feat(build): Fix build on latest alpine

--- cmake/variables.cmake
+++ cmake/variables.cmake
@@ -1,3 +1,5 @@
+set(POORPROF_SYSTEM_VCPKG FALSE CACHE BOOL "Use vcpkg from the system")
+
 set(POORPROF_UNWIND_PROVIDER "libdwfl;libunwind" CACHE STRING "Which unwinders to use: libdwfl, libunwind or libunwind-llvm")
 set(POORPROF_DEBUG_INFO_PROVIDER "libdwfl" CACHE STRING "Which debug info parser to use: libdwfl")
 
